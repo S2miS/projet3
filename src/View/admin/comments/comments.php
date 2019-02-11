2c74c4e4e6f9c5b3a8cacb1b0ea7ebc892fee61b
S2miS <simbox94@yahoo.fr>
2019-02-11 20:36:12 +0100

    début système commentaires

--- src/View/admin/comments/comments.php
+++ src/View/admin/comments/comments.php
@@ -10,24 +10,12 @@ ob_start();
             <div id="commcontent">
                 <p id="commtitle"><?= htmlspecialchars($data->getPseudo()) ?>
                     <span>Publié le : <?= htmlspecialchars($data->getDate()) ?></span></p>
-                <?php
-                if ($data->getModerate()== true){
-                    ?>
-                    <p>Ce commentaire a été modéré</p>
-                    <?php
-                }
-                else {
-                    ?>
                     <p id="commmessage"><?= htmlspecialchars($data->getMessage()) ?></p>
                     <?php
                 }
                 ?>
-                <a href="report&comm_id=<?= $data->getId() ?>&id=<?= $result->getId()?>">Report</a>
             </div>
         </div>
-        <?php
-        }
-        ?>
     </section>
 <?php
 $content = ob_get_clean();

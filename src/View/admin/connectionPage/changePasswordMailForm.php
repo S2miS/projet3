b79fcb63dbf5af90a38e479b02a215a3a974c39c
S2miS <simbox94@yahoo.fr>
2019-05-23 14:03:47 +0200

    Correction de bugs

--- src/View/admin/connectionPage/changePasswordMailForm.php
+++ src/View/admin/connectionPage/changePasswordMailForm.php
@@ -3,7 +3,7 @@ ob_start();
 ?>
 <div id="resetPassword">
 <form action="changement-mdp-email-check-email" method="post">
-    <label for="check-email">Votre email: </label>
+    <label for="check-email">Votre email : </label>
     <input type="email" id="check-email" name="check-email">
     <button type="submit">Confirmer</button>
 </form>
